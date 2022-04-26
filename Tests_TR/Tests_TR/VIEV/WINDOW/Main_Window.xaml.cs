5cb94be9171015fdf2ff1564c5f317cfd7d44f42
Гончаревич Евгений <evgenijgoncarevic@gmail.com>
2022-04-26 18:42:11 +0300

    Command fix

--- Tests_TR/Tests_TR/VIEV/WINDOW/Main_Window.xaml.cs
+++ Tests_TR/Tests_TR/VIEV/WINDOW/Main_Window.xaml.cs
@@ -1,19 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Tests_TR.VIEV.PAGES;
-using Tests_TR.VIEV_MODEL;
+﻿using System.Windows;
 namespace Tests_TR
 {
     /// <summary>
@@ -21,12 +6,10 @@ namespace Tests_TR
     /// </summary>
     public partial class MainWindow : Window
     {
-
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new MainWindowViewModel();
+            DataContext = new PagesVievModel();
         }
-
     }
 }
--- Tests_TR/Tests_TR/VIEV_MODEL/MainWindowViewModel.cs
+++ Tests_TR/Tests_TR/VIEV_MODEL/MainWindowViewModel.cs
@@ -1,45 +1,31 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
 using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Navigation;
-using Tests_TR.VIEV_MODEL;
 using Tests_TR.VIEV.PAGES;
-using System.Collections.Generic;
+using Tests_TR.VIEV_MODEL;
 
 namespace Tests_TR
 {
 
-    public abstract class Pages
+    public abstract class Pages_Props
     {
-        public static Login_Page login_Page = new();
-        public static Main_Page main_Page = new();
-        public static Page Current_Page { get; set; }
-        public static RelayCommand Switch_Page { get; set; }
+        private static readonly Login_Page login_Page = new();
+        private static readonly Main_Page main_Page = new();
+        private static readonly Testing_Page testing_page = new();
+        private static readonly Admin_Page admin_page = new();
+        public static Page Current_Page { get; set; } = login_Page;
+        public static List<Page> Pages_List { get; set; } = new() { login_Page, main_Page, testing_page, admin_page };
 
-        public static List<Page> Pages_List { get; set; } = new() { login_Page, main_Page };
-
-
-    }
-    public class MainWindowViewModel : Pages
-    {
-
-
-        public MainWindowViewModel()
-        {
-            Current_Page = login_Page;
-        }
+        internal static RelayCommand switch_Page = new(obj => { NavigationService.GetNavigationService(Current_Page).Navigate(obj); Current_Page = (Page)obj; });
+        public static RelayCommand? Switch_Page { get; set; }
 
     }
 
-    public class PagesVievModel : Pages
+    public class PagesVievModel : Pages_Props
     {
         public PagesVievModel()
         {
-            Switch_Page = new RelayCommand(obj =>
-            {
-                NavigationService.GetNavigationService(Current_Page).Navigate(obj);
-                Current_Page = (Page)obj;
-            });
+            Switch_Page = switch_Page;
         }
     }
 }
