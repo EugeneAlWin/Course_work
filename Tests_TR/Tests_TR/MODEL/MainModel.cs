e3258ac15ed8a03cdac829c1db21638d4b18a97b
Гончаревич Евгений <evgenijgoncarevic@gmail.com>
2022-05-03 16:10:13 +0300

    Test (Viev) fix

--- Tests_TR/Tests_TR/MODEL/MainModel.cs
+++ Tests_TR/Tests_TR/MODEL/MainModel.cs
@@ -3,10 +3,100 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#nullable disable
 namespace Tests_TR.MODEL
 {
-    internal class MainModel
+    public class MainModel
     {
     }
+
+    public class DatabaseContext : DbContext
+    {
+
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<Questions> Questions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DatabaseContext()
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=preCourse2;Trusted_Connection=True;");
+        }
+    }
+
+    public class Test
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Topic { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+        public List<Questions> Questions { get; set; } = new();
+
+    }
+
+    public class Questions
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string Question { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Answer_1 { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Answer_2 { get; set; }
+        [MaxLength(150)]
+        public string? Answer_3 { get; set; }
+        [MaxLength(150)]
+        public string? Answer_4 { get; set; }
+        [MaxLength(150)]
+        public string Right_Answer { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Image { get; set; }
+        public Test Test { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}-{Question}-{Answer_1}-{Answer_2}-{Answer_3}-{Answer_4}-{Right_Answer}-{Image}";
+        }
+    }
+
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Login { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Password { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Role { get; set; }
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [MaxLength(30)]
+        public string Last_Name { get; set; }
+        [MaxLength(30)]
+        public string Father_Name { get; set; }
+
+    }
+
 }
--- Tests_TR/Tests_TR/VIEV_MODEL/MainViewModel.cs
+++ Tests_TR/Tests_TR/VIEV_MODEL/MainViewModel.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
 using System;
+using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using Tests_TR.VIEV.PAGES;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Collections.ObjectModel;
-
+using Tests_TR.MODEL;
 namespace Tests_TR
 {
 
-    public partial class PagesVievModel
+    public partial class PagesVievModel //Props
     {
-        public static string[] Tests_List { get => tests_List; }
-        public static List<Page> Pages_List { get => pages_List; set => pages_List = value; }
-        public static ObservableCollection<int> SelectedIndex { get => selectedIndex; set => selectedIndex = value; }
 
+        public static string[] Tests_List { get => tests_List; }
+        public ObservableCollection<string> Login_Password { get => login_Password; set => login_Password = value; }
+        public static List<Page> Pages_List { get => pages_List; set => pages_List = value; }
+        public static ObservableCollection<Questions> Questions { get => questions; set => questions = value; }
+        public static ObservableCollection<int> SelectedIndex { get => selectedIndex; set => selectedIndex = value; }
         public static ObservableCollection<Style> Tab_Header_Color { get => tab_Header_Color; set => tab_Header_Color = value; }
 
         public static Page Current_Page { get => current_Page; set => current_Page = value; }
@@ -30,58 +33,96 @@ namespace Tests_TR
         }
     }
 
-    public partial class PagesVievModel : INotifyPropertyChanged
+
+    public partial class PagesVievModel : INotifyPropertyChanged //Fields
     {
 
         private static readonly Login_Page login_Page = new(); //Awailable page
         private static readonly Main_Page main_Page = new(); //Awailable page
         private static readonly Testing_Page testing_Page = new(); //Awailable page
         private static readonly Admin_Page admin_Page = new(); //Awailable page
+        private static Page current_Page = login_Page;
 
         private static readonly Style tab_Wrong = (Style)Application.Current.FindResource("Tab_Header_Wrong");
         private static readonly Style tab_Untouched = (Style)Application.Current.FindResource("Tab_Header_Untouched");
         private static readonly Style tab_Right = (Style)Application.Current.FindResource("Tab_Header_Right");
 
-        internal static readonly string[] tests_List = { "0", "1", "2", "3", "4", "5", "6", "7", }; //list of chapters (0=exam)
+        private static readonly string[] tests_List = { "0", "1", "2", "3", "4", "5", "6", "7", }; //list of chapters (0=exam)
 
-        internal static ObservableCollection<int> selectedIndex = new() { 0 }; //Index of tab
+        private static ObservableCollection<int> selectedIndex = new() { 0 }; //Index of tab
 
-        internal static ObservableCollection<Style> tab_Header_Color = new() { tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, };
+        private static ObservableCollection<Style> tab_Header_Color = new() { tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, tab_Untouched, };
+        private static ObservableCollection<string> login_Password = new() { "", "" };
+        private static ObservableCollection<Questions> questions = new();
 
-        internal static Page current_Page = login_Page;
-        internal static List<Page> pages_List = new() { login_Page, main_Page, testing_Page, admin_Page };
 
-        internal static readonly RelayCommand switch_Page = new(next_page => //Page navigation
-        {
-            //MessageBoxResult mbr = MessageBox.Show("Вы действительно хотите прервать тест?\nРезультат будет утерян.", "Выйти?", MessageBoxButton.YesNo);
-            NavigationService.GetNavigationService(current_Page).Navigate(next_page);
-            selectedIndex[0] = 0; current_Page = (Page)next_page;
-        });
+        private static List<Page> pages_List = new() { login_Page, main_Page, testing_Page, admin_Page };
 
-        internal static RelayCommand to_Test = new(num_of_test => //Tests navigation
-        {
-            NavigationService.GetNavigationService(current_Page).Navigate(testing_Page);
-            current_Page = testing_Page;
-            for (int i = 0; i <= 9; i++) tab_Header_Color.Insert(i, tab_Untouched);
-        });
-
-        internal static RelayCommand switch_Tab = new(next_tab => //Tab nav
-        {
-            var temp = Convert.ToBoolean(next_tab) ? ++selectedIndex[0] : --selectedIndex[0];
-            if (selectedIndex[0] > 9) selectedIndex[0] = 0;
-            if (selectedIndex[0] < 0) selectedIndex[0] = 9;
-        });
-        internal static RelayCommand switch_Tab_Color = new(curr_tub => //Tab color
-        {
-            var cur_tub_num = Convert.ToByte(curr_tub);
-            tab_Header_Color[cur_tub_num] = tab_Right;
-        });
-
+        private static readonly DatabaseContext db = new();
 
 
 
 
         public event PropertyChangedEventHandler? PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+
+
+    public partial class PagesVievModel //Commands' fields
+    {
+        private static readonly RelayCommand switch_Page = new(next_page => //Page navigation
+        {
+            //MessageBoxResult mbr = MessageBox.Show("Вы действительно хотите прервать тест?\nРезультат будет утерян.", "Выйти?", MessageBoxButton.YesNo);
+            if (next_page == login_Page)
+            {
+                login_Password[0] = login_Password[1] = "";
+            }
+            if (current_Page == login_Page)
+            {
+                if (login_Password[0] == "" || login_Password[1] == "") return;
+
+
+                var temp_Acsess = db.Users.Where(x => x.Login == login_Password[0] && x.Password == login_Password[1]).FirstOrDefault();
+
+                if (temp_Acsess != null)
+                {
+                    var temp_Role = db.Users.Where(p => p.Login == login_Password[0] && p.Password == login_Password[1]).Where(p => p.Role == "Admin").FirstOrDefault();
+                    MessageBox.Show(temp_Role != null ? "Вы вошли как администратор" : "Вы вошли как студент", "Авторизация прошла успешно!");
+                }
+                else MessageBox.Show("Неверно введен логин или пароль", "Ошибка авторизации!");
+            }
+            NavigationService.GetNavigationService(current_Page).Navigate(next_page);
+            selectedIndex[0] = 0; current_Page = (Page)next_page;
+        });
+
+        private static RelayCommand to_Test = new(num_of_test => //Tests (page) navigation
+        {
+            NavigationService.GetNavigationService(current_Page).Navigate(testing_Page);
+            current_Page = testing_Page;
+            for (int i = 0; i <= 9; i++) tab_Header_Color.Insert(i, tab_Untouched);
+            questions.Clear();
+
+            var temp = db.Questions.Where(p => p.Test.Id == Convert.ToInt32(num_of_test)).Count();
+            var random_Range = Enumerable.Range(0, temp).OrderBy(x => Guid.NewGuid()).Take(10).ToList();
+            var questions_Range = db.Questions.Where(p => p.Test.Id == Convert.ToInt32(num_of_test)).ToList();
+            foreach (var item in random_Range)
+            {
+                questions.Add(questions_Range[item]);
+            }
+
+
+        });
+
+        private static RelayCommand switch_Tab = new(next_tab => //Tab nav
+        {
+            var temp = Convert.ToBoolean(next_tab) ? ++selectedIndex[0] : --selectedIndex[0];
+            if (selectedIndex[0] > 9) selectedIndex[0] = 0;
+            if (selectedIndex[0] < 0) selectedIndex[0] = 9;
+        });
+        private static RelayCommand switch_Tab_Color = new(curr_tub => //Tab color
+        {
+            var cur_tub_num = Convert.ToByte(curr_tub);
+            tab_Header_Color[cur_tub_num] = tab_Right;
+        });
     }
 }
